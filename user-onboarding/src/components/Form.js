710c10a43e4e3c6554ebc72e3b37cc1e63022e42
Peggy Wollenhaupt <color.ui>
2020-02-20 14:21:15 -0700

    step 1 complete

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { withFormik, Form, Field } from "formik";
 
-const Form = () => {
-    return null;
+const UserForm = ({ values }) => {
+    return (
+        <Form>
+            <Field type="text" name="name" placeholder="Enter name" />
+            <br/>
+            <Field type="email" name="email" placeholder="Email address" />
+            <br/>
+            <Field type="password" name="password" placeholder="Password" />
+            <br/>
+            <label>
+                I Accept the Terms of Service
+                <Field
+                    type="checkbox"
+                    name="tos"
+                    checked={values.tos}
+                />
+            </label>
+            <br/>
+            <button type="submit">Submit</button>
+        </Form>
+    );
 }
 
-export default Form;
+const FormikUserForm = withFormik({
+    mapPropsToValues({ name, email, password, tos }) {
+        return {
+            name: name || "",
+            email: email || "",
+            password: password || "",
+            tos: tos || false
+        };
+    }
+})(UserForm);
+
+export default FormikUserForm;
